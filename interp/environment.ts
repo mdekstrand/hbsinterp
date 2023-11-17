40066697611767c17d3e994b8868e5ee726ed4b1
Michael Ekstrand <mdekstrand@drexel.edu>
2023-11-17 11:55:43 -0500

    refactor setup & allow tracing

--- interp/environment.ts
+++ interp/environment.ts
@@ -17,6 +17,7 @@ export type EnvSpec = {
   context?: Context;
   helpers?: HelperSet;
   partials?: Record<string, string> | PartialLookup;
+  trace: (msg: string, ...args: unknown[]) => void;
 };
 
 export function wrapPartials(set: Record<string, string>): PartialLookup {
@@ -37,6 +38,27 @@ export class Environment {
     this.helpers = help;
     this.partials = partials;
   }
+
+  static setup(spec: EnvSpec): Environment {
+    let partials = spec.partials;
+    if (typeof partials == "object") {
+      partials = wrapPartials(partials);
+    } else if (!partials) {
+      partials = () => undefined;
+    }
+    let env = new Environment(spec.context ?? {}, spec.helpers ?? {}, partials);
+    if (spec.trace) {
+      env.trace = spec.trace;
+    }
+    return env;
+  }
+
+  /**
+   * Trace template evaluation.
+   * @param _msg The message (with format specifiers, for console.log)
+   * @param _args The message arguments
+   */
+  trace(_msg: string, ..._args: unknown[]): void {}
 
   /**
    * Make an environment for an inner scope.  The scope is returned as a new
--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Context, Environment, EnvSpec, Helper, HelperSet, wrapPartials } from "./interp/environment.ts";
+import { Context, Environment, EnvSpec, Helper, HelperSet } from "./interp/environment.ts";
 import { AST, parseTemplate } from "./hbs.ts";
 import { interpretProgram } from "./interp/statement.ts";
 export { safe } from "./interp/strings.ts";
@@ -7,13 +7,7 @@ export { Environment };
 export type { Context, Helper, HelperSet };
 
 export function interpret(template: AST.Program | string, spec: EnvSpec): Promise<string> {
-  let partials = spec.partials;
-  if (typeof partials == "object") {
-    partials = wrapPartials(partials);
-  } else if (!partials) {
-    partials = () => undefined;
-  }
-  let env = new Environment(spec.context ?? {}, spec.helpers ?? {}, partials);
+  let env = Environment.setup(spec);
   if (typeof template == "string") {
     template = parseTemplate(template);
   }
