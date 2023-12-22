11c9735e45d7a9bc7664a7d5ba581b473de59899
Michael Ekstrand <md@ekstrandom.net>
2023-12-22 11:37:12 -0500

    whitespace control test pass

---
+++ tests/whitespace_test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { describe, it } from "std/testing/bdd.ts";
+
+import { interpret, safe } from "../mod.ts";
+
+describe("Whitespace control", () => {
+  it("should preserve whitespace", async () => {
+    let res = await interpret("< {{scroll}} >", { context: { scroll: "PRIRUTSENIE" } });
+    assertEquals(res, "< PRIRUTSENIE >");
+  });
+
+  it("should chomp right whitespace", async () => {
+    let res = await interpret("< {{scroll ~}} >", { context: { scroll: "PRIRUTSENIE" } });
+    assertEquals(res, "< PRIRUTSENIE>");
+  });
+
+  it("should chomp left whitespace", async () => {
+    let res = await interpret("< {{~ scroll}} >", { context: { scroll: "PRIRUTSENIE" } });
+    assertEquals(res, "<PRIRUTSENIE >");
+  });
+
+  it("should chomp both whitespace", async () => {
+    let res = await interpret("< {{~ scroll ~}} >", { context: { scroll: "PRIRUTSENIE" } });
+    assertEquals(res, "<PRIRUTSENIE>");
+  });
+});
